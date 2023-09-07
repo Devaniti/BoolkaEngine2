14fff550b29d6d019ff88f440026b87814bf9868
Dmytro Bulatov <dmytro.bulatov@boolka.dev>
2023-09-07 22:44:47 +0300

    Optimized clang-tidy

--- build_scripts/tidy.ps1
+++ build_scripts/tidy.ps1
@@ -1,6 +1,5 @@
 Push-Location $PSScriptRoot
-Remove-Item -LiteralPath "../tidy" -Force -Recurse -ErrorAction SilentlyContinue
-cmake -G "Ninja" -DCMAKE_CXX_CLANG_TIDY="clang-tidy" -S ../source -B ../tidy
+cmake -G "Ninja" -DCMAKE_CXX_CLANG_TIDY="clang-tidy" -DCMAKE_CXX_CLANG_TIDY_EXPORT_FIXES_DIR="$PSScriptRoot/../tidy/fixes" -S ../source -B ../tidy
 cmake --build ../tidy
-Remove-Item -LiteralPath "../tidy" -Force -Recurse
+clang-apply-replacements ../tidy/fixes
 Pop-Location
--- build_scripts/tidy_fix.ps1
+++
@@ -1,6 +0,0 @@
-Push-Location $PSScriptRoot
-Remove-Item -LiteralPath "../tidy" -Force -Recurse -ErrorAction SilentlyContinue
-cmake -G "Ninja" -DCMAKE_CXX_CLANG_TIDY="clang-tidy;--fix" -S ../source -B ../tidy
-cmake --build ../tidy
-Remove-Item -LiteralPath "../tidy" -Force -Recurse
-Pop-Location
